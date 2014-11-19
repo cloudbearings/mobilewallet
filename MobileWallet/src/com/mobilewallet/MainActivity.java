868a4f7867ce2dc2e2bb1dd1e2789e32b1156a97
cutegopichand <cutegopichand@gmail.com>
2014-11-19 17:56:38 +0000

    Added actionbar tabs and created dailyCredits activity

--- MobileWallet/src/com/mobilewallet/MainActivity.java
+++ MobileWallet/src/com/mobilewallet/MainActivity.java
@@ -1,13 +1,50 @@
 package com.mobilewallet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends ActionBarActivity{
+import com.mobilewallet.utils.Utils;
+
+public class MainActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-	}
 
+		super.onCreate(savedInstanceState);
+		try {
+			setContentView(R.layout.main_activity);
+
+			Button register = (Button) findViewById(R.id.register);
+			// Adding Helvetica custom font to button text
+			register.setTypeface(Utils.getFont(MainActivity.this,
+					getString(R.string.Helvetica)));
+			register.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					// Opening Registration activity
+					startActivity(new Intent(MainActivity.this,
+							RegisterActivity.class)
+							.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+				}
+			});
+
+			Button login = (Button) findViewById(R.id.login);
+			login.setTypeface(Utils.getFont(MainActivity.this,
+					getString(R.string.Helvetica)));
+			login.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					startActivity(new Intent(MainActivity.this,
+							LoginActivity.class)
+							.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- MobileWallet/src/com/mobilewallet/TabsActivity.java
+++ MobileWallet/src/com/mobilewallet/TabsActivity.java
@@ -1,14 +1,81 @@
 package com.mobilewallet;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar.Tab;
+import android.view.MenuItem;
+import com.mobilewallet.adapters.TabsAdapter;
 
-public class TabsActivity extends ActionBarActivity {
-	@Override
+public class TabsActivity extends ActionBarActivity implements
+		android.support.v7.app.ActionBar.TabListener {
+
+	private ActionBar actionBar;
+	private TabsAdapter mAdapter;
+	private String tabs[] = { "LEARN GK", "INVITE", "RECHARGE", "MORE" };
+	private ViewPager viewPager;
+
+	public TabsActivity() {
+	}
+
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.tabs);
+
+		// Specifying actionbar display options
+		getSupportActionBar().setDisplayOptions(
+				ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_HOME_AS_UP
+						| ActionBar.DISPLAY_SHOW_HOME);
+		// Adding custom actionbar
+		getSupportActionBar().setCustomView(R.layout.custom_actionbar);
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getSupportActionBar();
+		mAdapter = new TabsAdapter(getSupportFragmentManager());
+		viewPager.setAdapter(mAdapter);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// Adding Tabs
+		for (String tab_name : tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tab_name)
+					.setTabListener(this));
+		}
+
+		/**
+		 * on swiping the viewpager make respective tab selected
+		 * */
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				actionBar.setSelectedNavigationItem(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
+
+	}
+
+	@Override
+	public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction arg1) {
+		viewPager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
+
 	}
 
 }
