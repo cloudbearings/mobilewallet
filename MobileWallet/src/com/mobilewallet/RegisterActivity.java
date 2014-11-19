8dd74ff7d823b1bb19cc8f5ae4ef6e54065df3ae
cutegopichand@gmail.com <cutegopichand@gmail.com@c318d4ca-1c1b-bf94-8fde-030cc43690c3>
2014-11-19 14:54:59 +0000

    Added  layout to activity

--- MobileWallet/src/com/mobilewallet/RegisterActivity.java
+++ MobileWallet/src/com/mobilewallet/RegisterActivity.java
@@ -3,11 +3,12 @@ package com.mobilewallet;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
-public class RegisterActivity extends ActionBarActivity{
+public class RegisterActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.register_activity);
 	}
 
 }
