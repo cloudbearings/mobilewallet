098ecc6c191481f1e395ed924698aa4b1673a6d2
cutegopichand@gmail.com <cutegopichand@gmail.com@c318d4ca-1c1b-bf94-8fde-030cc43690c3>
2014-11-19 17:18:51 +0000

    Created adapter to show fragments in actionbar tabs

---
+++ MobileWallet/src/com/mobilewallet/adapters/TabsAdapter.java
@@ -0,0 +1,41 @@
+// Decompiled by Jad v1.5.8e. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.geocities.com/kpdus/jad.html
+// Decompiler options: braces fieldsfirst space lnc 
+
+package com.mobilewallet.adapters;
+
+import android.support.v4.app.FixedFragmentStatePagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import com.mobilewallet.LearnGkFragment;
+import com.mobilewallet.MoreFragment;
+import com.mobilewallet.RechargeFragment;
+import com.mobilewallet.users.InviteFragment;
+
+public class TabsAdapter extends FixedFragmentStatePagerAdapter {
+
+	public TabsAdapter(FragmentManager fragmentmanager) {
+		super(fragmentmanager);
+	}
+
+	public int getCount() {
+		return 4;
+	}
+
+	public Fragment getItem(int i) {
+		switch (i) {
+		
+		case 0:
+			return new LearnGkFragment();
+		case 1:
+			return new InviteFragment();
+		case 2:
+			return new RechargeFragment();
+		case 3:
+			return new MoreFragment();
+		default:
+			return null;
+
+		}
+	}
+}
