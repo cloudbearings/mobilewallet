e64b5f49f11dbf6ee5c5853b9d773873d0cffadb
cutegopichand <cutegopichand@gmail.com>
2014-11-19 15:25:24 +0000

    Added service urls

--- MobileWallet/src/com/mobilewallet/service/BuildServices.java
+++ MobileWallet/src/com/mobilewallet/service/BuildServices.java
@@ -1,5 +1,13 @@
 package com.mobilewallet.service;
 
+import retrofit.RestAdapter;
+
 public class BuildServices {
+
+	String URL = "http://www.mobilewalletapp.com";
+
+	MobileWalletService build = new RestAdapter.Builder().setEndpoint(URL)
+			.setConverter(new StringConverter()).build()
+			.create(MobileWalletService.class);
 
 }
