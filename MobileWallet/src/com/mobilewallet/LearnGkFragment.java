966788086fda514375f1487089667e4a04f5cb69
cutegopichand <cutegopichand@gmail.com>
2014-11-19 14:42:31 +0000

    Added learnGk layout to fragment

--- MobileWallet/src/com/mobilewallet/LearnGkFragment.java
+++ MobileWallet/src/com/mobilewallet/LearnGkFragment.java
@@ -1,13 +1,19 @@
 package com.mobilewallet;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class LearnGkFragment extends ActionBarActivity {
+public class LearnGkFragment extends Fragment {
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.learn_gk_fragment, container,
+				false);
+		return view;
 	}
 
 }
