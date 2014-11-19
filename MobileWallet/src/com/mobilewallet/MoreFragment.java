e38aba7ee721e429163bfbb65b114bb2eefad1cb
cutegopichand@gmail.com <cutegopichand@gmail.com@c318d4ca-1c1b-bf94-8fde-030cc43690c3>
2014-11-19 18:07:48 +0000

    Created Balance, Notifications and MyProfile Activity classes

--- MobileWallet/src/com/mobilewallet/MoreFragment.java
+++ MobileWallet/src/com/mobilewallet/MoreFragment.java
@@ -1,17 +1,108 @@
 package com.mobilewallet;
 
+import com.mobilewallet.users.BalanceActivity;
+import com.mobilewallet.users.MyProfile;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MoreFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater,
 			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.more_fragment, container, false);
+
+		ImageView wallet = (ImageView) view.findViewById(R.id.wallet);
+		wallet.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getActivity(), BalanceActivity.class)
+						.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+
+			}
+		});
+
+		ImageView dialyCredits = (ImageView) view
+				.findViewById(R.id.dailyCredits);
+		dialyCredits.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getActivity(), DailyCredits.class)
+						.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+
+			}
+		});
+
+		ImageView notification = (ImageView) view
+				.findViewById(R.id.notification);
+		notification.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getActivity(), Notifications.class)
+						.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+
+			}
+		});
+
+		ImageView profile = (ImageView) view.findViewById(R.id.profile);
+		profile.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getActivity(), MyProfile.class)
+						.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+
+			}
+		});
+
+		ImageView share = (ImageView) view.findViewById(R.id.share);
+		share.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		ImageView rate = (ImageView) view.findViewById(R.id.rate);
+		rate.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+		ImageView feedBack = (ImageView) view.findViewById(R.id.feedBack);
+		feedBack.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getActivity(), Feedback.class)
+						.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+			}
+		});
+
+		ImageView about = (ImageView) view.findViewById(R.id.about);
+		about.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getActivity(), About.class)
+						.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+
+			}
+		});
+
 		return view;
 	}
 }
--- MobileWallet/src/com/mobilewallet/users/MyProfile.java
+++ MobileWallet/src/com/mobilewallet/users/MyProfile.java
@@ -1,22 +1,27 @@
-package com.mobilewallet;
+package com.mobilewallet.users;
+
+import com.mobilewallet.R;
+import com.mobilewallet.R.id;
+import com.mobilewallet.R.layout;
+import com.mobilewallet.R.menu;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class BalanceActivity extends ActionBarActivity {
+public class MyProfile extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.balance_activity);
+		setContentView(R.layout.my_profile);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.balance, menu);
+		getMenuInflater().inflate(R.menu.my_profile, menu);
 		return true;
 	}
 
