2320f50a2855bb0a46567a4f1bf28da889a7455b
cutegopichand@gmail.com <cutegopichand@gmail.com@c318d4ca-1c1b-bf94-8fde-030cc43690c3>
2014-11-28 12:14:02 +0000

    Modified code

--- MobileWallet/src/com/mobilewallet/ForgotPassword.java
+++ MobileWallet/src/com/mobilewallet/ForgotPassword.java
@@ -1,13 +1,26 @@
 package com.mobilewallet;
 
+import android.app.Activity;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.view.MotionEvent;
 
-public class ForgotPassword extends ActionBarActivity {
+public class ForgotPassword extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.forgot_password);
+	}
+
+	@Override
+	public boolean dispatchTouchEvent(MotionEvent ev) {
+		Rect dialogBounds = new Rect();
+		getWindow().getDecorView().getHitRect(dialogBounds);
+
+		if (!dialogBounds.contains((int) ev.getX(), (int) ev.getY())) {
+			return false;
+		}
+		return super.dispatchTouchEvent(ev);
 	}
 }
