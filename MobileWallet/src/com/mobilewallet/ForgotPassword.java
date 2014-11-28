96ec894ffddb88ed5ee3eae106c22efc68078f92
cutegopichand <cutegopichand@gmail.com>
2014-11-28 13:08:14 +0000

    Modified forgot pwd css

--- MobileWallet/src/com/mobilewallet/ForgotPassword.java
+++ MobileWallet/src/com/mobilewallet/ForgotPassword.java
@@ -1,9 +1,13 @@
 package com.mobilewallet;
 
+import com.mobilewallet.utils.Utils;
+
 import android.app.Activity;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.TextView;
 
 public class ForgotPassword extends Activity {
 	@Override
@@ -11,6 +15,10 @@ public class ForgotPassword extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.forgot_password);
+
+		TextView heading = (TextView) findViewById(R.id.textView2);
+		heading.setTypeface(Utils.getFont(ForgotPassword.this, getString(R.string.Helvetica)),
+				Typeface.BOLD);
 	}
 
 	@Override
