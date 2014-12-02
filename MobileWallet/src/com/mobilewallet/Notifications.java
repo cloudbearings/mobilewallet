5d157c198e2ac95255a5087c797be7e027ada128
cutegopichand <cutegopichand@gmail.com>
2014-12-02 07:25:46 +0000

    Added code for Notifications activity

--- MobileWallet/src/com/mobilewallet/Notifications.java
+++ MobileWallet/src/com/mobilewallet/Notifications.java
@@ -1,34 +1,142 @@
 package com.mobilewallet;
 
-import android.support.v7.app.ActionBarActivity;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+import com.mobilewallet.googleanalytics.MobileWalletGoogleAnalytics;
+import com.mobilewallet.googleanalytics.MobileWalletGoogleAnalytics.TrackerName;
+import com.mobilewallet.service.BuildService;
+import com.mobilewallet.utils.Utils;
 
 public class Notifications extends ActionBarActivity {
+
+	private CompoundButton offerSwitch;
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		try {
+			Tracker t = ((MobileWalletGoogleAnalytics) getApplication())
+					.getTracker(TrackerName.APP_TRACKER);
+			t.setScreenName(getString(R.string.notifications_screen_name));
+			t.send(new HitBuilders.AppViewBuilder().build());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.notifications);
+		try {
+			setContentView(R.layout.notifications);
+			offerSwitch = (CompoundButton) findViewById(R.id.notificationsStauts);
+			offerSwitch.setTypeface(
+					Utils.getFont(Notifications.this, getString(R.string.GothamRnd)),
+					Typeface.BOLD);
+
+			populateDefaultData();
+			offerSwitchChangedListener();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.notifications, menu);
-		return true;
+	private void populateDefaultData() {
+		BuildService.build.notification(Utils.getUserId(Notifications.this),
+				new Callback<String>() {
+
+					@Override
+					public void success(String result, Response arg1) {
+						// TODO Auto-generated method stub
+						try {
+							Log.i("notification json", result + " result");
+							JSONArray array = new JSONArray(result);
+
+							offerSwitch.setChecked("Y".equals((String) array.get(0)));
+
+						} catch (Exception e) {
+							e.printStackTrace();
+							offerSwitch.setChecked(false);
+
+						}
+
+					}
+
+					@Override
+					public void failure(RetrofitError arg0) {
+						// TODO Auto-generated method stub
+						arg0.printStackTrace();
+						offerSwitch.setChecked(false);
+
+					}
+				});
+
+	}
+
+	private void offerSwitchChangedListener() {
+		offerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				updateNotification(getString(R.string.offer), isChecked ? "Y" : "N");
+			}
+		});
+	}
+
+	private void updateNotification(String type, String status) {
+		BuildService.build.notification(Utils.getUserId(Notifications.this), status, type,
+				new Callback<String>() {
+					@Override
+					public void failure(RetrofitError arg0) {
+						arg0.printStackTrace();
+						showToad(getString(R.string.notifc_failure_msg));
+					}
+
+					@Override
+					public void success(String result, Response arg1) {
+						try {
+							if (!"Y".equals(new JSONObject(result).getString("sc"))) {
+								showToad(getString(R.string.notifc_failure_msg));
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+							showToad(getString(R.string.notifc_failure_msg));
+						}
+					}
+				});
+
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent tabsIntent = new Intent(Notifications.this, TabsActivity.class);
+			tabsIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(tabsIntent);
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-		return super.onOptionsItemSelected(item);
+	}
+
+	private void showToad(String toad) {
+		Toast.makeText(Notifications.this, toad, Toast.LENGTH_LONG).show();
+
 	}
 }
