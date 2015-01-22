f3f5cbb784b53994cbe2756c09d2a07614c353c5
cutegopichand@gmail.com <cutegopichand@gmail.com@c318d4ca-1c1b-bf94-8fde-030cc43690c3>
2015-01-22 15:46:11 +0000

    Added code to store TagPosts and SearhPosts in sqlite.

--- TestingAndroidApp/src/com/wordlypost/SearchPost.java
+++ TestingAndroidApp/src/com/wordlypost/SearchPost.java
@@ -1,6 +1,7 @@
 package com.wordlypost;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -16,8 +17,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 
@@ -26,6 +27,7 @@ import com.google.android.gms.analytics.Tracker;
 import com.wordlypost.WordlyPostGoogleAnalytics.TrackerName;
 import com.wordlypost.adapters.PostAdapter;
 import com.wordlypost.beans.PostRowItem;
+import com.wordlypost.dao.SearchPostsDAO;
 import com.wordlypost.service.BuildService;
 import com.wordlypost.utils.Utils;
 
@@ -37,6 +39,7 @@ public class SearchPost extends ActionBarActivity {
 	private long page = 1;
 	private PostAdapter adapter;
 	private ProgressBar progressBar;
+	private SearchPostsDAO searchPostsDAO;
 
 	@Override
 	public void onResume() {
@@ -60,7 +63,8 @@ public class SearchPost extends ActionBarActivity {
 		rowItems = new ArrayList<PostRowItem>();
 		searchPostsList = (ListView) findViewById(R.id.searchPostsList);
 		progressBar = (ProgressBar) findViewById(R.id.searchPostsProgressBar);
-		adapter = new PostAdapter(SearchPost.this, R.layout.post_list_item, rowItems);
+		adapter = new PostAdapter(SearchPost.this, R.layout.post_list_item,
+				rowItems);
 		searchPostsList.setAdapter(adapter);
 
 		searchPostsList.setOnScrollListener(new OnScrollListener() {
@@ -70,111 +74,190 @@ public class SearchPost extends ActionBarActivity {
 			}
 
 			@Override
-			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
-					int totalItemCount) {
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
 
 				int lastInScreen = firstVisibleItem + visibleItemCount;
 				if ((lastInScreen == totalItemCount) && !(loadingMore)) {
 					if (Utils.isNetworkAvailable(SearchPost.this)) {
-						if (getIntent().getStringExtra("search") != null) {
-							loadingMore = true;
-							progressBar.setVisibility(View.VISIBLE);
-
-							BuildService.build.searchPosts(getIntent().getStringExtra("search"),
-									page, new Callback<String>() {
-
-										@Override
-										public void failure(RetrofitError retofitError) {
-											retofitError.printStackTrace();
-										}
-
-										@Override
-										public void success(String output, Response res) {
-
-											try {
-												Log.i("searchPosts", res.getUrl());
-												Log.i("searchPosts", output);
-
-												JSONObject obj = new JSONObject(output);
-
-												if (obj.getJSONArray("posts").length() > 0) {
-													JSONArray categotyPosts = obj
-															.getJSONArray("posts");
-
-													for (int i = 0; i < categotyPosts.length(); i++) {
-														JSONObject categotyPost = categotyPosts
-																.getJSONObject(i);
-
-														PostRowItem item = new PostRowItem();
-
-														item.setPost_id(categotyPost.getInt("id"));
-														item.setTitle(categotyPost
-																.getString("title"));
-														item.setDate(categotyPost.getString("date"));
-														item.setPost_icon_url(categotyPost
-																.getString("thumbnail"));
-														item.setAuthor(categotyPost.getJSONObject(
-																"author").getString("name"));
-														item.setContent(categotyPost
-																.getString("content"));
-														item.setPost_banner(categotyPost
-																.getJSONObject("thumbnail_images")
-																.getJSONObject("full")
-																.getString("url"));
-														item.setComment_count(categotyPost
-																.getInt("comment_count"));
-														item.setPost_url(categotyPost
-																.getString("url"));
-														item.setPost_des(categotyPost
-																.getString("excerpt"));
-
-														if (categotyPost.getInt("comment_count") > 0) {
-															item.setCommentsArray(categotyPost
-																	.getJSONArray("comments")
-																	.toString());
-														}
-														item.setTagsArray(categotyPost
-																.getJSONArray("tags").toString());
-
-														rowItems.add(item);
-													}
-
-													adapter.notifyDataSetChanged();
-													loadingMore = false;
-
-													if (page == obj.getInt("pages")) {
-														Log.i("loadingmore", "true");
-														loadingMore = true;
-													} else {
-														page = page + 1;
-													}
-												} else {
-													Utils.displayToad(SearchPost.this,
-															getString(R.string.no_posts_error_msg));
-												}
-												progressBar.setVisibility(View.GONE);
-
-											} catch (Exception e) {
-												progressBar.setVisibility(View.GONE);
-												e.printStackTrace();
-											}
-										}
-									});
-						}
+						getDataFromServer();
 					} else {
-						Utils.displayToad(SearchPost.this, getString(R.string.no_internet));
+						getDataFromSqlite();
 					}
 				}
 			}
 		});
 
 		searchPostsList.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				startActivity(new Intent(SearchPost.this, PostViewSwipeActivity.class)
-						.putExtra("postDetails", rowItems).putExtra("position", position)
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				startActivity(new Intent(SearchPost.this,
+						PostViewSwipeActivity.class)
+						.putExtra("postDetails", rowItems)
+						.putExtra("position", position)
 						.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
 			}
 		});
+	}
+
+	private void getDataFromServer() {
+		if (getIntent().getStringExtra("search") != null) {
+			loadingMore = true;
+			progressBar.setVisibility(View.VISIBLE);
+
+			BuildService.build.searchPosts(
+					getIntent().getStringExtra("search"), page,
+					new Callback<String>() {
+
+						@Override
+						public void failure(RetrofitError retofitError) {
+							retofitError.printStackTrace();
+						}
+
+						@Override
+						public void success(String output, Response res) {
+
+							try {
+								Log.i("searchPosts", res.getUrl());
+								Log.i("searchPosts", output);
+
+								JSONObject obj = new JSONObject(output);
+
+								if (obj.getJSONArray("posts").length() > 0) {
+									JSONArray categotyPosts = obj
+											.getJSONArray("posts");
+									String currentMilliSeconds = Calendar
+											.getInstance().getTimeInMillis()
+											+ "";
+
+									for (int i = 0; i < categotyPosts.length(); i++) {
+										JSONObject categotyPost = categotyPosts
+												.getJSONObject(i);
+
+										PostRowItem item = new PostRowItem();
+
+										item.setPost_id(categotyPost
+												.getInt("id"));
+										item.setTitle(categotyPost
+												.getString("title"));
+										item.setDate(categotyPost
+												.getString("date"));
+										item.setPost_icon_url(categotyPost
+												.getString("thumbnail"));
+										item.setAuthor(categotyPost
+												.getJSONObject("author")
+												.getString("name"));
+										item.setContent(categotyPost
+												.getString("content"));
+										item.setPost_banner(categotyPost
+												.getJSONObject(
+														"thumbnail_images")
+												.getJSONObject("full")
+												.getString("url"));
+										item.setComment_count(categotyPost
+												.getInt("comment_count"));
+										item.setPost_url(categotyPost
+												.getString("url"));
+										item.setPost_des(categotyPost
+												.getString("excerpt"));
+
+										if (categotyPost
+												.getInt("comment_count") > 0) {
+											item.setCommentsArray(categotyPost
+													.getJSONArray("comments")
+													.toString());
+										}
+										item.setTagsArray(categotyPost
+												.getJSONArray("tags")
+												.toString());
+
+										rowItems.add(item);
+
+										if (page == 1) {
+											searchPostsDAO = new SearchPostsDAO(
+													SearchPost.this);
+											searchPostsDAO.insertSearchPosts(
+													categotyPost.getInt("id"),
+													categotyPost
+															.getString("title"),
+													categotyPost
+															.getString("date"),
+													categotyPost
+															.getString("thumbnail"),
+													categotyPost.getJSONObject(
+															"author")
+															.getString("name"),
+													categotyPost
+															.getString("content"),
+													categotyPost
+															.getJSONObject(
+																	"thumbnail_images")
+															.getJSONObject(
+																	"full")
+															.getString("url"),
+													categotyPost
+															.getInt("comment_count"),
+													categotyPost
+															.getString("url"),
+													currentMilliSeconds,
+													categotyPost
+															.getString("excerpt"),
+													categotyPost.getJSONArray(
+															"comments")
+															.toString(),
+													categotyPost.getJSONArray(
+															"tags").toString());
+										}
+									}
+
+									if (page == 1) {
+										long deleted = searchPostsDAO
+												.deleteSearchPosts(currentMilliSeconds);
+										Log.i("Deleted Records: ", deleted + "");
+									}
+
+									adapter.notifyDataSetChanged();
+									loadingMore = false;
+
+									if (page == obj.getInt("pages")) {
+										Log.i("loadingmore", "true");
+										loadingMore = true;
+									} else {
+										page = page + 1;
+									}
+								} else {
+									Utils.displayToad(
+											SearchPost.this,
+											getString(R.string.no_posts_error_msg));
+								}
+								progressBar.setVisibility(View.GONE);
+
+							} catch (Exception e) {
+								progressBar.setVisibility(View.GONE);
+								e.printStackTrace();
+							}
+						}
+					});
+		}
+	}
+
+	private void getDataFromSqlite() {
+		loadingMore = true;
+		progressBar.setVisibility(View.VISIBLE);
+		searchPostsDAO = new SearchPostsDAO(SearchPost.this);
+
+		if (searchPostsDAO.getSearchPostsCount() > 0) {
+			rowItems = searchPostsDAO.getSearchPosts();
+			adapter = new PostAdapter(SearchPost.this, R.layout.post_list_item,
+					rowItems);
+
+			searchPostsList.setAdapter(adapter);
+			adapter.notifyDataSetChanged();
+		} else {
+			Utils.displayToad(SearchPost.this,
+					getString(R.string.no_posts_error_msg));
+		}
+		progressBar.setVisibility(View.GONE);
 	}
 
 	@Override
---
+++ TestingAndroidApp/src/com/wordlypost/dao/SearchPostsDAO.java
@@ -0,0 +1,196 @@
+package com.wordlypost.dao;
+
+import java.util.ArrayList;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
+
+import com.wordlypost.beans.PostRowItem;
+import com.wordlypost.persistance.DbAdapter;
+
+public class SearchPostsDAO {
+
+	private SQLiteDatabase database;
+	private DbAdapter dbHelper;
+	private Context context;
+
+	public SearchPostsDAO(Context context) {
+		this.context = context;
+	}
+
+	public SearchPostsDAO opnToWrite() {
+		dbHelper = new DbAdapter(context);
+		database = dbHelper.getWritableDatabase();
+		return this;
+	}
+
+	public void Close() {
+		database.close();
+	}
+
+	public long insertSearchPosts(int post_id, String post_title,
+			String post_date, String post_icon_url, String post_author_name,
+			String post_content, String post_screen_image_url,
+			int post_comment_count, String post_url,
+			String current_milliseconds, String excerpt, String comments,
+			String tags) {
+		ContentValues cv = new ContentValues(13);
+		cv.put(DbAdapter.SP_ID, post_id);
+		cv.put(DbAdapter.SP_TITLE, post_title);
+		cv.put(DbAdapter.SP_DATE, post_date);
+		cv.put(DbAdapter.SP_ICON_URL, post_icon_url);
+		cv.put(DbAdapter.SP_AUTHOR_NAME, post_author_name);
+		cv.put(DbAdapter.SP_CONTENT, post_content);
+		cv.put(DbAdapter.SP_SCREEN_IMAGE_URL, post_screen_image_url);
+		cv.put(DbAdapter.SP_COMMENT_COUNT, post_comment_count);
+		cv.put(DbAdapter.SP_URL, post_url);
+		cv.put(DbAdapter.SP_CURRENT_MILLISECONDS, current_milliseconds);
+		cv.put(DbAdapter.SP_EXCERPT, excerpt);
+		cv.put(DbAdapter.SP_COMMENTS, comments);
+		cv.put(DbAdapter.SP_TAGS, tags);
+
+		opnToWrite();
+		long val = 0;
+		if (isSearchPostExists(post_id) > 0) {
+			val = database.update(DbAdapter.SEARCH_POSTS_TABLE_NAME, cv,
+					DbAdapter.SP_ID + "=" + post_id, null);
+			Close();
+		} else {
+			val = database.insert(DbAdapter.SEARCH_POSTS_TABLE_NAME, null, cv);
+			Close();
+		}
+
+		return val;
+	}
+
+	public long isSearchPostExists(int post_id) {
+		SQLiteDatabase database = null;
+		long count = 0;
+		try {
+			String sql = "SELECT COUNT(*) FROM "
+					+ DbAdapter.SEARCH_POSTS_TABLE_NAME + " where "
+					+ DbAdapter.SP_ID + "=" + post_id;
+
+			dbHelper = new DbAdapter(context);
+			database = dbHelper.getReadableDatabase();
+			SQLiteStatement statement = database.compileStatement(sql);
+			count = statement.simpleQueryForLong();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				database.close();
+			} catch (Exception e) {
+			}
+		}
+		return count;
+	}
+
+	public long getSearchPostsCount() {
+		SQLiteDatabase database = null;
+		Cursor c = null;
+		long count = 0;
+		try {
+			String[] cols = { DbAdapter.SP_ID };
+
+			dbHelper = new DbAdapter(context);
+			database = dbHelper.getReadableDatabase();
+			c = database.query(DbAdapter.SEARCH_POSTS_TABLE_NAME, cols, null,
+					null, null, null, null);
+			count = c.getCount();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (!c.isClosed()) {
+					c.close();
+				}
+			} catch (Exception e) {
+			}
+			try {
+				database.close();
+			} catch (Exception e) {
+			}
+		}
+		return count;
+	}
+
+	public ArrayList<PostRowItem> getSearchPosts() {
+		SQLiteDatabase database = null;
+		Cursor cursor = null;
+		ArrayList<PostRowItem> postsList = null;
+		try {
+			postsList = new ArrayList<PostRowItem>();
+			String[] cols = { DbAdapter.SP_ID, DbAdapter.SP_TITLE,
+					DbAdapter.SP_DATE, DbAdapter.SP_ICON_URL,
+					DbAdapter.SP_AUTHOR_NAME, DbAdapter.SP_CONTENT,
+					DbAdapter.SP_SCREEN_IMAGE_URL, DbAdapter.SP_COMMENT_COUNT,
+					DbAdapter.SP_URL, DbAdapter.SP_EXCERPT,
+					DbAdapter.SP_COMMENTS, DbAdapter.SP_TAGS };
+
+			dbHelper = new DbAdapter(context);
+			database = dbHelper.getReadableDatabase();
+			cursor = database.query(DbAdapter.SEARCH_POSTS_TABLE_NAME, cols, null,
+					null, null, null, DbAdapter.SP_DATE + " ASC");
+			PostRowItem item;
+			if (cursor.moveToFirst()) {
+				do {
+					item = new PostRowItem();
+
+					item.setPost_id(cursor.getInt(cursor
+							.getColumnIndex(DbAdapter.SP_ID)));
+					item.setTitle(cursor.getString(cursor
+							.getColumnIndex(DbAdapter.SP_TITLE)));
+					item.setDate(cursor.getString(cursor
+							.getColumnIndex(DbAdapter.SP_DATE)));
+					item.setPost_icon_url(cursor.getString(cursor
+							.getColumnIndex(DbAdapter.SP_ICON_URL)));
+					item.setAuthor(cursor.getString(cursor
+							.getColumnIndex(DbAdapter.SP_AUTHOR_NAME)));
+					item.setContent(cursor.getString(cursor
+							.getColumnIndex(DbAdapter.SP_CONTENT)));
+					item.setPost_banner(cursor.getString(cursor
+							.getColumnIndex(DbAdapter.SP_SCREEN_IMAGE_URL)));
+					item.setComment_count(cursor.getInt(cursor
+							.getColumnIndex(DbAdapter.SP_COMMENT_COUNT)));
+					item.setPost_url(cursor.getString(cursor
+							.getColumnIndex(DbAdapter.SP_URL)));
+					item.setPost_des(cursor.getString(cursor
+							.getColumnIndex(DbAdapter.SP_EXCERPT)));
+					item.setCommentsArray(cursor.getString(cursor
+							.getColumnIndex(DbAdapter.SP_COMMENTS)));
+					item.setTagsArray(cursor.getString(cursor
+							.getColumnIndex(DbAdapter.SP_TAGS)));
+
+					postsList.add(item);
+				} while (cursor.moveToNext());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (!cursor.isClosed()) {
+					cursor.close();
+				}
+			} catch (Exception e) {
+			}
+			try {
+				database.close();
+			} catch (Exception e) {
+			}
+		}
+		return postsList;
+	}
+
+	public long deleteSearchPosts(String currentMilliseconds) {
+		opnToWrite();
+		long val = database.delete(DbAdapter.SEARCH_POSTS_TABLE_NAME,
+				DbAdapter.SP_CURRENT_MILLISECONDS + "!='" + currentMilliseconds
+						+ "'", null);
+		Close();
+		return val;
+	}
+}
