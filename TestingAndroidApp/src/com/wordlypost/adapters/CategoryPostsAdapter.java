cbdcab7c3732a2fa8b0aa20c8e1fe6c9a3af979b
cutegopichand@gmail.com <cutegopichand@gmail.com@c318d4ca-1c1b-bf94-8fde-030cc43690c3>
2015-01-22 15:16:14 +0000

    Removed custom font code

--- TestingAndroidApp/src/com/wordlypost/adapters/CategoryPostsAdapter.java
+++ TestingAndroidApp/src/com/wordlypost/adapters/CategoryPostsAdapter.java
@@ -6,7 +6,6 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,7 +18,6 @@ import com.wordlypost.PostViewSwipeActivity;
 import com.wordlypost.R;
 import com.wordlypost.beans.PostRowItem;
 import com.wordlypost.utils.ImageLoader;
-import com.wordlypost.utils.Utils;
 
 public class CategoryPostsAdapter extends ArrayAdapter<PostRowItem> {
 
@@ -28,7 +26,8 @@ public class CategoryPostsAdapter extends ArrayAdapter<PostRowItem> {
 
 	private ArrayList<PostRowItem> items;
 
-	public CategoryPostsAdapter(Context context, int resourceId, ArrayList<PostRowItem> items) {
+	public CategoryPostsAdapter(Context context, int resourceId,
+			ArrayList<PostRowItem> items) {
 		super(context, resourceId, items);
 		this.context = context;
 		imgLoader = new ImageLoader(context.getApplicationContext());
@@ -50,31 +49,33 @@ public class CategoryPostsAdapter extends ArrayAdapter<PostRowItem> {
 				.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
 		if (myAppsView == null) {
 
-			myAppsView = mInflater.inflate(R.layout.category_posts_list_item, parent, false);
+			myAppsView = mInflater.inflate(R.layout.category_posts_list_item,
+					parent, false);
 
 			holder = new ViewHolder();
 
 			holder.title = (TextView) myAppsView.findViewById(R.id.title);
 			holder.date = (TextView) myAppsView.findViewById(R.id.date);
-			holder.post_image = (ImageView) myAppsView.findViewById(R.id.post_image);
+			holder.post_image = (ImageView) myAppsView
+					.findViewById(R.id.post_image);
 
 			myAppsView.setTag(holder);
 		} else
 			holder = (ViewHolder) myAppsView.getTag();
 
 		holder.title.setText(Html.fromHtml(rowItem.getTitle()));
-		holder.title.setTypeface(Utils.getFont(context, context.getString(R.string.Helvetica)),
-				Typeface.BOLD);
 		holder.date.setText(rowItem.getDate());
-		holder.date.setTypeface(Utils.getFont(context, context.getString(R.string.DroidSerif)));
 
 		int loader = R.drawable.app_default_icon;
-		imgLoader.DisplayImage(rowItem.getPost_icon_url(), loader, holder.post_image);
+		imgLoader.DisplayImage(rowItem.getPost_icon_url(), loader,
+				holder.post_image);
 
 		holder.post_image.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
-				context.startActivity(new Intent(context, PostViewSwipeActivity.class)
-						.putExtra("postDetails", items).putExtra("position", position)
+				context.startActivity(new Intent(context,
+						PostViewSwipeActivity.class)
+						.putExtra("postDetails", items)
+						.putExtra("position", position)
 						.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
 			}
 		});
