cb33bdf857acb9b8d4f846660df67f181520248a
cutegopichand@gmail.com <cutegopichand@gmail.com@c318d4ca-1c1b-bf94-8fde-030cc43690c3>
2015-03-13 16:57:40 +0000

    Added code to show Question activity

--- MobileWallet/src/com/mobilewallet/LearnGkFragment.java
+++ MobileWallet/src/com/mobilewallet/LearnGkFragment.java
@@ -1,19 +1,31 @@
 package com.mobilewallet;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class LearnGkFragment extends Fragment {
 	@Override
-	public View onCreateView(LayoutInflater inflater,
-			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.learn_gk_fragment, container,
 				false);
+		try {
+			ImageView learnGK = (ImageView) view.findViewById(R.id.learn_GK);
+			learnGK.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View view) {
+
+					startActivity(new Intent(getActivity(), Question.class)
+							.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+				}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return view;
 	}
-
 }
