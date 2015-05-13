3227b4706973e277e8513583a36d533ca6e0aed2
cutegopichand@gmail.com <cutegopichand@gmail.com@c318d4ca-1c1b-bf94-8fde-030cc43690c3>
2015-05-13 16:09:03 +0000

    Modified code to show updated posts from server in home fragment

--- TestingAndroidApp/src/com/wordlypost/HomePostsSwipeViewActivity.java
+++ TestingAndroidApp/src/com/wordlypost/HomePostsSwipeViewActivity.java
@@ -20,6 +20,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.wordlypost.adapters.PageViewSwipeAdapter;
 import com.wordlypost.beans.PostRowItem;
@@ -38,59 +40,61 @@ public class HomePostsSwipeViewActivity extends ActionBarActivity {
 	private ArrayList<PostRowItem> posts;
 	private long page = 1;
 	private boolean loadingMore = false;
+	private HomeDAO homeDAO;
+	private ProgressBar progressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.post_view_swipe);
+		setContentView(R.layout.home_post_view);
 		try {
 
+			progressBar = (ProgressBar) findViewById(R.id.home_progress_bar);
 			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 			fragments = new Vector<Fragment>();
 
-			ArrayList<PostRowItem> rowItems = (ArrayList<PostRowItem>) getIntent().getSerializableExtra("postDetails");
-			if (rowItems.size() > 0) {
-				generateOffers(rowItems);
-			}
+			getPostsFromServer();
+			/*
+			 * ArrayList<PostRowItem> rowItems = (ArrayList<PostRowItem>)
+			 * getIntent().getSerializableExtra("postDetails"); if
+			 * (rowItems.size() > 0) { generateOffers(rowItems); }
+			 */
 
 			// Initilization
 			viewPager = (ViewPager) findViewById(R.id.postViewPager);
 
-			mAdapter = new PageViewSwipeAdapter(super.getSupportFragmentManager(), fragments);
+			mAdapter = new PageViewSwipeAdapter(
+					super.getSupportFragmentManager(), fragments);
 
 			viewPager.setAdapter(mAdapter);
 
-			showTab(getIntent().getIntExtra("position", 0));
-			
 			posts = new ArrayList<PostRowItem>();
 
 			/**
 			 * on swiping the viewpager make respective tab selected
 			 * */
-			viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			viewPager
+					.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
-				@Override
-				public void onPageSelected(int position) {
-					addPostUrl(position);
-					Log.i("scrolled", position + "");
-					/*if (position == 4) {
-						if (!loadingMore) {
-							loadingMore = true;
-							getPostsFromServer();
+						@Override
+						public void onPageSelected(int position) {
+							 addPostUrl(position);
+							if (position == posts.size()) {
+								if (!loadingMore) {
+									getPostsFromServer();
+								}
+							}
 						}
-					}*/
-				}
 
-				@Override
-				public void onPageScrolled(int pos, float arg1, int arg2) {
-					/* Log.i("scrolled", pos+""); */
-				}
+						@Override
+						public void onPageScrolled(int pos, float arg1, int arg2) {
+						}
 
-				@Override
-				public void onPageScrollStateChanged(int arg0) {
-				}
-			});
+						@Override
+						public void onPageScrollStateChanged(int arg0) {
+						}
+					});
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -101,87 +105,172 @@ public class HomePostsSwipeViewActivity extends ActionBarActivity {
 				&& getIntent().getStringExtra("slug") != null) {
 			final int categoryId = getIntent().getIntExtra("categoryId", 0);
 			final String sulg = getIntent().getStringExtra("slug");
-			BuildService.build.getCategoriesPosts(categoryId, sulg, page, new Callback<String>() {
+			homeDAO = new HomeDAO(HomePostsSwipeViewActivity.this);
+			if (page == 1
+					&& Utils.callPostsUrl(HomePostsSwipeViewActivity.this)
+					&& homeDAO.getPostsCount(categoryId, sulg) > 6) {
+				posts = homeDAO.getPosts(categoryId, sulg);
+				generateOffers(posts);
+				showTab(getIntent().getIntExtra("position", 0));
+			} else {
+				progressBar.setVisibility(View.VISIBLE);
+				loadingMore = true;
 
-				@Override
-				public void success(String output, Response arg1) {
-					try {
-						Log.i("Home Post View :", arg1.getUrl());
-						Log.i("Home Post View :", output);
-						JSONObject obj = new JSONObject(output);
+				BuildService.build.getCategoriesPosts(categoryId, sulg, page,
+						new Callback<String>() {
 
-						if (obj.getJSONArray("posts").length() > 0) {
-							HomeDAO homeDAO = new HomeDAO(HomePostsSwipeViewActivity.this);
-							JSONArray categotyPosts = obj.getJSONArray("posts");
-							String currentMilliSeconds = Calendar.getInstance().getTimeInMillis()
-									+ "";
+							@Override
+							public void success(String output, Response arg1) {
+								try {
+									Log.i("Home Post View :", arg1.getUrl());
+									Log.i("Home Post View :", output);
+									JSONObject obj = new JSONObject(output);
 
-							for (int i = 5; i < categotyPosts.length(); i++) {
-								JSONObject categotyPost = categotyPosts.getJSONObject(i);
+									if (obj.getJSONArray("posts").length() > 0) {
 
-								PostRowItem item = new PostRowItem();
+										Utils.storePostsLoadedDate(HomePostsSwipeViewActivity.this);
 
-								item.setPost_id(categotyPost.getInt("id"));
-								item.setTitle(categotyPost.getString("title"));
-								item.setDate(categotyPost.getString("date"));
-								item.setPost_icon_url(categotyPost.getString("thumbnail"));
-								item.setAuthor(categotyPost.getJSONObject("author").getString(
-										"name"));
-								item.setContent(categotyPost.getString("content"));
-								item.setPost_banner(categotyPost.getJSONObject("thumbnail_images")
-										.getJSONObject("full").getString("url"));
-								item.setComment_count(categotyPost.getInt("comment_count"));
-								item.setPost_url(categotyPost.getString("url"));
+										JSONArray categotyPosts = obj
+												.getJSONArray("posts");
+										String currentMilliSeconds = Calendar
+												.getInstance()
+												.getTimeInMillis()
+												+ "";
 
-								if (categotyPost.getInt("comment_count") > 0) {
-									item.setCommentsArray(categotyPost.getJSONArray("comments")
-											.toString());
+										for (int i = 0; i < categotyPosts
+												.length(); i++) {
+											JSONObject categotyPost = categotyPosts
+													.getJSONObject(i);
+
+											PostRowItem item = new PostRowItem();
+
+											item.setPost_id(categotyPost
+													.getInt("id"));
+											item.setTitle(categotyPost
+													.getString("title"));
+											item.setDate(categotyPost
+													.getString("date"));
+											item.setPost_icon_url(categotyPost
+													.getString("thumbnail"));
+											item.setAuthor(categotyPost
+													.getJSONObject("author")
+													.getString("name"));
+											item.setContent(categotyPost
+													.getString("content"));
+											item.setPost_banner(categotyPost
+													.getJSONObject(
+															"thumbnail_images")
+													.getJSONObject("full")
+													.getString("url"));
+											item.setComment_count(categotyPost
+													.getInt("comment_count"));
+											item.setPost_url(categotyPost
+													.getString("url"));
+
+											if (categotyPost
+													.getInt("comment_count") > 0) {
+												item.setCommentsArray(categotyPost
+														.getJSONArray(
+																"comments")
+														.toString());
+											}
+											item.setTagsArray(categotyPost
+													.getJSONArray("tags")
+													.toString());
+											item.setPost_des(categotyPost
+													.getString("excerpt"));
+
+											posts.add(item);
+
+											if (page == 1) {
+												/*
+												 * Inserting category posts in
+												 * sqlite.
+												 */
+												homeDAO.insertPosts(
+														categotyPost
+																.getInt("id"),
+														categotyPost
+																.getString("title"),
+														categotyPost
+																.getString("date"),
+														categotyPost
+																.getString("thumbnail"),
+														categotyPost
+																.getJSONObject(
+																		"author")
+																.getString(
+																		"name"),
+														categotyPost
+																.getString("content"),
+														categotyPost
+																.getJSONObject(
+																		"thumbnail_images")
+																.getJSONObject(
+																		"full")
+																.getString(
+																		"url"),
+														categotyPost
+																.getInt("comment_count"),
+														categotyPost
+																.getString("url"),
+														currentMilliSeconds,
+														categotyPost
+																.getString("excerpt"),
+														categoryId,
+														sulg,
+														"",
+														categotyPost
+																.getJSONArray(
+																		"comments")
+																.toString(),
+														categotyPost
+																.getJSONArray(
+																		"tags")
+																.toString());
+											}
+										}
+
+										if (page == 1) {
+											long deleted = homeDAO.deletePosts(
+													categoryId, sulg,
+													currentMilliSeconds);
+											Log.i("Deleted records: ", deleted
+													+ "");
+										}
+
+										generateOffers(posts);
+										progressBar.setVisibility(View.GONE);
+										mAdapter.notifyDataSetChanged();
+										showTab(getIntent().getIntExtra(
+												"position", 0));
+
+										if (page == obj.getInt("pages")) {
+											Log.i("loadingmore", "true");
+											loadingMore = true;
+										} else {
+											page = page + 1;
+											loadingMore = false;
+										}
+
+									} else {
+										Utils.displayToad(
+												HomePostsSwipeViewActivity.this,
+												getString(R.string.no_posts_error_msg));
+									}
+								} catch (Exception e) {
+									Log.d(TAG,
+											"Exception raised to get category posts from server.");
+									e.printStackTrace();
 								}
-								item.setTagsArray(categotyPost.getJSONArray("tags").toString());
-								item.setPost_des(categotyPost.getString("excerpt"));
-
-								posts.add(item);
-
-								/*
-								 * Inserting category posts in sqlite.
-								 */
-								homeDAO.insertPosts(categotyPost.getInt("id"), categotyPost
-										.getString("title"), categotyPost.getString("date"),
-										categotyPost.getString("thumbnail"), categotyPost
-												.getJSONObject("author").getString("name"),
-										categotyPost.getString("content"),
-										categotyPost.getJSONObject("thumbnail_images")
-												.getJSONObject("full").getString("url"),
-										categotyPost.getInt("comment_count"), categotyPost
-												.getString("url"), currentMilliSeconds,
-										categotyPost.getString("excerpt"), categoryId, sulg, "",
-										categotyPost.getJSONArray("comments").toString(),
-										categotyPost.getJSONArray("tags").toString());
-
 							}
 
-							long deleted = homeDAO.deletePosts(categoryId, sulg,
-									currentMilliSeconds);
-							Log.i("Deleted records: ", deleted + "");
-
-							generateOffers(posts);
-							mAdapter.notifyDataSetChanged();
-
-						} else {
-							Utils.displayToad(HomePostsSwipeViewActivity.this,
-									getString(R.string.no_posts_error_msg));
-						}
-					} catch (Exception e) {
-						Log.d(TAG, "Exception raised to get category posts from server.");
-						e.printStackTrace();
-					}
-				}
-
-				@Override
-				public void failure(RetrofitError retrofitError) {
-					retrofitError.printStackTrace();
-				}
-			});
+							@Override
+							public void failure(RetrofitError retrofitError) {
+								retrofitError.printStackTrace();
+							}
+						});
+			}
 		}
 	}
 
@@ -220,15 +309,16 @@ public class HomePostsSwipeViewActivity extends ActionBarActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			Intent tabsIntent = new Intent(HomePostsSwipeViewActivity.this, TabsActivity.class);
+			Intent tabsIntent = new Intent(HomePostsSwipeViewActivity.this,
+					TabsActivity.class);
 			tabsIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 			startActivity(tabsIntent);
 			return true;
 		case R.id.share_icon:
 			Intent sendIntent = new Intent();
 			sendIntent.setAction(Intent.ACTION_SEND);
-			sendIntent.putExtra(Intent.EXTRA_TEXT, postUrl).putExtra(Intent.EXTRA_SUBJECT,
-					postTitle);
+			sendIntent.putExtra(Intent.EXTRA_TEXT, postUrl).putExtra(
+					Intent.EXTRA_SUBJECT, postTitle);
 			sendIntent.setType("text/plain");
 			startActivity(sendIntent);
 			return true;
