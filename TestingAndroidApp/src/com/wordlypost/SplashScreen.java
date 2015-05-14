922e162525a881b752d92b48e3652746a99451f7
cutegopichand@gmail.com <cutegopichand@gmail.com@c318d4ca-1c1b-bf94-8fde-030cc43690c3>
2015-05-14 15:16:21 +0000

    Added splash time out

--- TestingAndroidApp/src/com/wordlypost/SplashScreen.java
+++ TestingAndroidApp/src/com/wordlypost/SplashScreen.java
@@ -55,11 +55,11 @@ public class SplashScreen extends ActionBarActivity {
 			setContentView(R.layout.splash_screen);
 
 			TextView appDes1 = (TextView) findViewById(R.id.app_des1);
-			appDes1.setTypeface(
-					Utils.getFont(SplashScreen.this, getString(R.string.DroidSerif_Bold)),
-					Typeface.BOLD);
+			appDes1.setTypeface(Utils.getFont(SplashScreen.this,
+					getString(R.string.DroidSerif_Bold)), Typeface.BOLD);
 			TextView appDes2 = (TextView) findViewById(R.id.app_des2);
-			appDes2.setTypeface(Utils.getFont(SplashScreen.this, getString(R.string.DroidSerif)));
+			appDes2.setTypeface(Utils.getFont(SplashScreen.this,
+					getString(R.string.DroidSerif)));
 
 			if (Utils.isNetworkAvailable(SplashScreen.this)) {
 				categoriesDAO = new CategoriesDAO(SplashScreen.this);
@@ -68,9 +68,16 @@ public class SplashScreen extends ActionBarActivity {
 				 */
 				if (categoriesDAO.getCategoriesCount() > 0) {
 					// getRandomCategories();
-					startActivity(new Intent(SplashScreen.this, TabsActivity.class)
-							.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-					finish();
+					new Handler().postDelayed(new Runnable() {
+
+						@Override
+						public void run() {
+							startActivity(new Intent(SplashScreen.this,
+									TabsActivity.class)
+									.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+							finish();
+						}
+					}, SPLASH_TIME_OUT);
 				} else {
 					BuildService.build.getCategories(new Callback<String>() {
 
@@ -79,7 +86,8 @@ public class SplashScreen extends ActionBarActivity {
 							try {
 								Log.i("Categories :", output);
 								JSONObject obj = new JSONObject(output);
-								if (obj.getString("status").equals(getString(R.string.error))) {
+								if (obj.getString("status").equals(
+										getString(R.string.error))) {
 									Utils.displayToad(SplashScreen.this,
 											getString(R.string.task_error_msg));
 									finish();
@@ -91,9 +99,10 @@ public class SplashScreen extends ActionBarActivity {
 								categories = output;
 								JSONObject obj = new JSONObject(output);
 
-								for (int i = 0; i < obj.getJSONArray("categories").length(); i++) {
-									JSONObject category = obj.getJSONArray("categories")
-											.getJSONObject(i);
+								for (int i = 0; i < obj.getJSONArray(
+										"categories").length(); i++) {
+									JSONObject category = obj.getJSONArray(
+											"categories").getJSONObject(i);
 									/*
 									 * If server categories count is greater
 									 * than sqlite db categories count then
@@ -109,11 +118,13 @@ public class SplashScreen extends ActionBarActivity {
 										 * statement is used to stop storing
 										 * BestVideosEver in sqlite database.
 										 */
-										if (category.getInt("id") != getResources().getInteger(
-												R.integer.best_vides_ever)) {
+										if (category.getInt("id") != getResources()
+												.getInteger(
+														R.integer.best_vides_ever)) {
 											String isHomeCategory;
-											if (category.getInt("id") == getResources().getInteger(
-													R.integer.top_news)
+											if (category.getInt("id") == getResources()
+													.getInteger(
+															R.integer.top_news)
 													|| i < 5) {
 												// Log.i("count", i + "");
 												isHomeCategory = getString(R.string.Y);
@@ -121,10 +132,12 @@ public class SplashScreen extends ActionBarActivity {
 												isHomeCategory = getString(R.string.N);
 											}
 
-											categoriesDAO.insertCategories(category.getInt("id"),
+											categoriesDAO.insertCategories(
+													category.getInt("id"),
 													category.getString("title"),
 													category.getString("slug"),
-													category.getInt("post_count"), isHomeCategory);
+													category.getInt("post_count"),
+													isHomeCategory);
 										}
 									}
 								}
@@ -141,9 +154,16 @@ public class SplashScreen extends ActionBarActivity {
 					});
 				}
 			} else {
-				startActivity(new Intent(SplashScreen.this, TabsActivity.class)
-						.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-				finish();
+				new Handler().postDelayed(new Runnable() {
+
+					@Override
+					public void run() {
+						startActivity(new Intent(SplashScreen.this,
+								TabsActivity.class)
+								.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+						finish();
+					}
+				}, SPLASH_TIME_OUT);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -168,7 +188,8 @@ public class SplashScreen extends ActionBarActivity {
 				}
 			}, SPLASH_TIME_OUT);
 		} else {
-			List<NavDrawerItem> categories = categoriesDAO.getRandomCategories();
+			List<NavDrawerItem> categories = categoriesDAO
+					.getRandomCategories();
 			for (int i = 0; i < categories.size(); i++) {
 				NavDrawerItem category = categories.get(i);
 				getCategoryPosts(category.getId(), category.getSlug());
@@ -177,59 +198,78 @@ public class SplashScreen extends ActionBarActivity {
 	}
 
 	private void getCategoryPosts(final int categoryId, final String slug) {
-		BuildService.build.getCategoriesPosts(categoryId, slug, 1, new Callback<String>() {
+		BuildService.build.getCategoriesPosts(categoryId, slug, 1,
+				new Callback<String>() {
 
-			@Override
-			public void failure(RetrofitError retrofitError) {
-				retrofitError.printStackTrace();
-			}
+					@Override
+					public void failure(RetrofitError retrofitError) {
+						retrofitError.printStackTrace();
+					}
 
-			@Override
-			public void success(String output, Response arg1) {
-				try {
-					Log.i("Category Posts :", output);
-					HomeDAO homeDAO = new HomeDAO(SplashScreen.this);
-					JSONObject obj = new JSONObject(output);
-					String categoryTitle = obj.getJSONObject("category").getString("title");
-					if (obj.getJSONArray("posts").length() > 0) {
-						JSONArray categotyPosts = obj.getJSONArray("posts");
-						String currentMilliSeconds = Calendar.getInstance().getTimeInMillis() + "";
-						for (int i = 0; i < categotyPosts.length(); i++) {
-							JSONObject categotyPost = categotyPosts.getJSONObject(i);
-							homeDAO.insertPosts(
-									categotyPost.getInt("id"),
-									categotyPost.getString("title"),
-									categotyPost.getString("date"),
-									categotyPost.getString("thumbnail"),
-									categotyPost.getJSONObject("author").getString("name"),
-									categotyPost.getString("content"),
-									categotyPost.getJSONObject("thumbnail_images")
-											.getJSONObject("full").getString("url"), categotyPost
-											.getInt("comment_count"),
-									categotyPost.getString("url"), currentMilliSeconds,
-									categotyPost.getString("excerpt"), categoryId, slug,
-									categoryTitle,
-									categotyPost.getJSONArray("comments").toString(), categotyPost
-											.getJSONArray("tags").toString());
-						}
+					@Override
+					public void success(String output, Response arg1) {
+						try {
+							Log.i("Category Posts :", output);
+							HomeDAO homeDAO = new HomeDAO(SplashScreen.this);
+							JSONObject obj = new JSONObject(output);
+							String categoryTitle = obj
+									.getJSONObject("category").getString(
+											"title");
+							if (obj.getJSONArray("posts").length() > 0) {
+								JSONArray categotyPosts = obj
+										.getJSONArray("posts");
+								String currentMilliSeconds = Calendar
+										.getInstance().getTimeInMillis() + "";
+								for (int i = 0; i < categotyPosts.length(); i++) {
+									JSONObject categotyPost = categotyPosts
+											.getJSONObject(i);
+									homeDAO.insertPosts(
+											categotyPost.getInt("id"),
+											categotyPost.getString("title"),
+											categotyPost.getString("date"),
+											categotyPost.getString("thumbnail"),
+											categotyPost
+													.getJSONObject("author")
+													.getString("name"),
+											categotyPost.getString("content"),
+											categotyPost
+													.getJSONObject(
+															"thumbnail_images")
+													.getJSONObject("full")
+													.getString("url"),
+											categotyPost
+													.getInt("comment_count"),
+											categotyPost.getString("url"),
+											currentMilliSeconds,
+											categotyPost.getString("excerpt"),
+											categoryId,
+											slug,
+											categoryTitle,
+											categotyPost.getJSONArray(
+													"comments").toString(),
+											categotyPost.getJSONArray("tags")
+													.toString());
+								}
 
-						long deleted = homeDAO.deletePosts(categoryId, slug, currentMilliSeconds);
-						Log.i("Deleted records: ", deleted + "");
-						count++;
-						if (count == 5) {
-							openTabsActivity();
+								long deleted = homeDAO.deletePosts(categoryId,
+										slug, currentMilliSeconds);
+								Log.i("Deleted records: ", deleted + "");
+								count++;
+								if (count == 5) {
+									openTabsActivity();
+								}
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
 						}
 					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+				});
 	}
 
 	private void openTabsActivity() {
-		startActivity(new Intent(SplashScreen.this, TabsActivity.class).addFlags(
-				Intent.FLAG_ACTIVITY_CLEAR_TOP).putExtra("categories", categories));
+		startActivity(new Intent(SplashScreen.this, TabsActivity.class)
+				.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP).putExtra(
+						"categories", categories));
 		finish();
 	}
 }
