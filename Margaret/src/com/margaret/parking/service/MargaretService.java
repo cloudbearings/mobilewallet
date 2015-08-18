3e4655e3e538dbc9183cec377792947c215fe950
cutegopichand <cutegopichand@gmail.com>
2015-08-18 15:10:42 +0000

    Modified services

--- Margaret/src/com/margaret/parking/service/MargaretService.java
+++ Margaret/src/com/margaret/parking/service/MargaretService.java
@@ -16,24 +16,24 @@ public interface MargaretService {
 	void getAllComplaints(@Query("pageindex") int pageindex,
 			@Query("pagesize") int pagesize, Callback<String> callback);
 
-	@POST("/values/AuthenticateUser")
+	@POST("/values/authenticateuserNew")
 	void authenticateUser(@Query("username") String uname,
 			@Query("password") String pwd, Callback<String> callback);
 
-	@GET("values/sendNewComplaints")
+	@GET("/values/sendNewComplaints")
 	void sendNewcomplaints(@Query("complaints") JSONArray uname,
 			Callback<String> callback);
 
-	@GET("values/GetAllComplaintsByTowings")
+	@GET("/values/GetAllComplaintsByTowings")
 	void getAllComplaintsByTowings(Callback<String> callback);
 
-	@GET("values/GetAllComplaintsByclamps")
+	@GET("/values/GetAllComplaintsByclamps")
 	void getAllComplaintsByclamps(Callback<String> callback);
 
-	@GET("values/getbyuserid")
+	@GET("/values/getbyuserid")
 	void getbyuserid(@Query("id") String id, Callback<String> callback);
 
-	@GET("values/GetCustomersByFNameLName")
+	@GET("/values/GetCustomersByFNameLName")
 	void getCustomersByFNameLName(@Query("searchname") String searchname, Callback<String> callback);
 
 }
